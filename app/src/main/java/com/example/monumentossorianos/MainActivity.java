6b4177f69c532a1d576020de3a74b11153700fd9
Ivan <ivanamor98@hotmail.com>
2021-11-11 17:26:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/monumentossorianos/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.monumentossorianos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeView(View v) {
+        setContentView(R.layout.create_monument);
+    }
+}
